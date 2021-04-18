09fff7f1e47455ef3cbae28426815d23af5ecd0a
paedayz <pae16phasit@gmail.com>
2021-04-18 13:41:16 +0700

    basic go lang

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for index, item := range cards {
+		fmt.Println(index, item)
+	}
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
